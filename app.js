8697ec6e19fa620c384d36f3a5c17037c838f1de
john ontita <johnontita2000@gmail.com>
2024-06-02 22:15:47 +0300

    using addEventlistening property to handle events

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log(document.querySelector('.message').textContent)
+document.querySelector('.message').textContent='🌷correct score'
+// handling events
+document.querySelector('.btn-two').addEventListener('click',function(){
+    console.log(document.querySelector('.input').value)
+})
+//Again! button
+document.querySelector('.btn-one').addEventListener('click',function(){
+    console.log(document.querySelector('.input').value)
+
+}
+)
